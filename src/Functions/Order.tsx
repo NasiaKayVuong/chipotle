f93133902a8cd8efa79f9decd39821b98d09dbff
NasiaVuong <nasia@enklu.com>
2022-05-12 00:23:46 -0700

    updated ui and changes state handling

--- src/Functions/Order.tsx
+++ src/Functions/Order.tsx
@@ -1,38 +1,26 @@
-import { useState, useEffect, Fragment, ChangeEvent } from "react";
-// import RandomOrder from "./RandomOrder";
+import { useState, useEffect, Fragment, ChangeEvent, MouseEvent } from 'react';
 
 function Order() {
-  // const [protein, setProtein] = useState({});
-  // const [bean, setBean] = useState({});
-  // const [rice, setRice] = useState({});
-  // const [sauce, setSauce] = useState({});
-  // const [toppings, setToppings] = useState({});
-  // const [vegetables, setVegetables] = useState({});
-  const [order, setOrder] = useState("");
-  // const [reqProtein, setReqProtein] = useState(false);
-  // const [reqBeans, setReqBeans] = useState(false);
-  // const [reqRice, setReqRice] = useState(false);
-  // const [isDisabled, setIsDisabled] = useState(true);
-  const [orderList, setOrderList] = useState([""]);
+  const [order, setOrder] = useState<any | null>(null);
 
   const proteinOptions = [
-    "chicken",
-    "pollo asado",
-    "steak",
-    "barbacoa",
-    "carnitas",
-    "sofritas"
+    'chicken',
+    'pollo asado',
+    'steak',
+    'barbacoa',
+    'carnitas',
+    'sofritas',
   ];
-  const beanOptions = ["pinto beans", "black beans"];
-  const riceOptions = ["white rice", "brown rice"];
+  const beanOptions = ['pinto beans', 'black beans'];
+  const riceOptions = ['white rice', 'brown rice'];
   const sauceOptions = [
-    "pico de gallo",
-    "queso blanco",
-    "tomatillo-green chili salsa",
-    "tomatillo-red chili salsa"
+    'pico de gallo',
+    'queso blanco',
+    'tomatillo-green chili salsa',
+    'tomatillo-red chili salsa',
   ];
-  const toppingsOptions = ["cheese", "sour cream", "guacamole"];
-  const vegetablesOptions = ["fajita veggies", "corn salsa", "lettuce"];
+  const toppingsOptions = ['cheese', 'sour cream', 'guacamole'];
+  const vegetablesOptions = ['fajita veggies', 'corn salsa', 'lettuce'];
 
   const menuOptions = [
     proteinOptions,
@@ -40,52 +28,20 @@ function Order() {
     riceOptions,
     sauceOptions,
     toppingsOptions,
-    vegetablesOptions
+    vegetablesOptions,
   ];
 
   function handleCheckbox() {
-    var checkBox = document.getElementById("checkbox") as HTMLInputElement;
+    var checkBox = document.getElementById('checkbox') as HTMLInputElement;
 
     if (checkBox) {
       checkBox.checked = !checkBox.checked;
     }
   }
 
-  // function checkForRequiredItems(sectionName: string) {
-  //   let section = document.getElementById(sectionName) as HTMLElement;
-  //   let mapChildren = section.querySelectorAll("input");
-  //   let somethingChecked = false;
-  //   for (let i = 0; i < mapChildren.length; i++) {
-  //     if (mapChildren[i].checked) {
-  //       somethingChecked = true;
-  //     }
-  //   }
-  //   switch (sectionName) {
-  //     case "protein":
-  //       setReqProtein(somethingChecked);
-  //       console.log(reqProtein);
-  //       break;
-  //     case "bean":
-  //       setReqBeans(somethingChecked);
-  //       console.log(reqBeans);
-  //       break;
-  //     case "rice":
-  //       setReqRice(somethingChecked);
-  //       console.log(reqRice);
-  //       break;
-  //   }
-
-  //   // setIsDisabled(reqProtein && reqBeans && reqRice ? false : true);
-
-  //   return reqProtein && reqBeans && reqRice;
-  // }
-
-  // function handleOnChange(sectionName: string) {
-  //   handleCheckbox();
-  //   setIsDisabled(!checkForRequiredItems(sectionName));
-  // }
-
-  function sendChoices(e) {
+  function sendChoices(
+    e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
+  ) {
     e.preventDefault();
     let choices = [];
     for (let i = 0; i < menuOptions.length; i++) {
@@ -100,7 +56,8 @@ function Order() {
       }
     }
 
-    setOrder(choices.join(", "));
+    setOrder(choices);
+    console.log(choices);
     return choices;
   }
 
@@ -108,138 +65,143 @@ function Order() {
     <Fragment>
       {/* <RandomOrder /> */}
       <form>
-        <div className="row">
-          <h2 className="text-left">Protein Options🥩</h2>
-          <div id="protein" className="flex justify-start ">
+        <div className='row'>
+          <h2 className='text-left'>Protein Options🥩</h2>
+          <div id='protein' className='flex justify-start '>
             {proteinOptions.map((item, i) => (
               <div key={i}>
-                <label className="inline-flex items-center mt-3">
+                <label className='inline-flex items-center mt-3'>
                   <input
                     id={item}
-                    type="checkbox"
-                    className="form-checkbox h-5 w-5 text-gray-600"
+                    type='checkbox'
+                    className='form-checkbox h-5 w-5 text-gray-600'
                     onChange={() => {
                       handleCheckbox();
                     }}
                   />
-                  <span className="ml-2 text-gray-700">{item}</span>
+                  <span className='ml-2 text-gray-700'>{item}</span>
                 </label>
               </div>
             ))}
           </div>
-          <h2 className="text-left">Bean Options 🌯</h2>
-          <div id="bean" className="flex justify-start ">
+          <h2 className='text-left'>Bean Options 🌯</h2>
+          <div id='bean' className='flex justify-start '>
             {beanOptions.map((item, i) => (
               <div key={i}>
-                <label className="inline-flex items-center mt-3">
+                <label className='inline-flex items-center mt-3'>
                   <input
                     id={item}
-                    type="checkbox"
-                    className="form-checkbox h-5 w-5 text-gray-600"
+                    type='checkbox'
+                    className='form-checkbox h-5 w-5 text-gray-600'
                     onChange={() => {
                       handleCheckbox();
                     }}
                   />
-                  <span className="ml-2 text-gray-700">{item}</span>
+                  <span className='ml-2 text-gray-700'>{item}</span>
                 </label>
               </div>
             ))}
           </div>
 
-          <h2 className="text-left">Rice Options 🍚</h2>
-          <div id="rice" className="flex justify-start ">
+          <h2 className='text-left'>Rice Options 🍚</h2>
+          <div id='rice' className='flex justify-start '>
             {riceOptions.map((item, i) => (
               <div key={i}>
-                <label className="inline-flex items-center mt-3">
+                <label className='inline-flex items-center mt-3'>
                   <input
                     id={item}
-                    type="checkbox"
-                    className="form-checkbox h-5 w-5 text-gray-600"
-                    // onChange={() => {
-                    //   handleCheckbox();
-                    // }}
-                    onChange={handleCheckbox}
+                    type='checkbox'
+                    className='form-checkbox h-5 w-5 text-gray-600'
+                    onChange={() => {
+                      handleCheckbox();
+                    }}
                   />
-                  <span className="ml-2 text-gray-700">{item}</span>
+                  <span className='ml-2 text-gray-700'>{item}</span>
                 </label>
               </div>
             ))}
           </div>
 
-          <h2 className="text-left">Sauce Options 🍯</h2>
-          <div id="sauce" className="flex justify-start ">
+          <h2 className='text-left'>Sauce Options 🍯</h2>
+          <div id='sauce' className='flex justify-start '>
             {sauceOptions.map((item, i) => (
               <div key={i}>
-                <label className="inline-flex items-center mt-3">
+                <label className='inline-flex items-center mt-3'>
                   <input
                     id={item}
-                    type="checkbox"
-                    className="form-checkbox h-5 w-5 text-gray-600"
-                    // onChange={() => {
-                    //   handleCheckbox();
-                    // }}
-                    onChange={handleCheckbox}
+                    type='checkbox'
+                    className='form-checkbox h-5 w-5 text-gray-600'
+                    onChange={() => {
+                      handleCheckbox();
+                    }}
                   />
-                  <span className="ml-2 text-gray-700">{item}</span>
+                  <span className='ml-2 text-gray-700'>{item}</span>
                 </label>
               </div>
             ))}
           </div>
 
-          <h2 className="text-left">Topping Options 🧀</h2>
-          <div id="toppings" className="flex justify-start ">
+          <h2 className='text-left'>Topping Options 🧀</h2>
+          <div id='toppings' className='flex justify-start '>
             {toppingsOptions.map((item, i) => (
               <div key={i}>
-                <label className="inline-flex items-center mt-3">
+                <label className='inline-flex items-center mt-3'>
                   <input
                     id={item}
-                    type="checkbox"
-                    className="form-checkbox h-5 w-5 text-gray-600"
-                    // onChange={() => {
-                    //   handleCheckbox();
-                    // }}
-                    onChange={handleCheckbox}
+                    type='checkbox'
+                    className='form-checkbox h-5 w-5 text-gray-600'
+                    onChange={() => {
+                      handleCheckbox();
+                    }}
                   />
-                  <span className="ml-2 text-gray-700">{item}</span>
+                  <span className='ml-2 text-gray-700'>{item}</span>
                 </label>
               </div>
             ))}
           </div>
 
-          <h2 className="text-left">Vegetable Options 🥬</h2>
-          <div id="vegetables" className="flex justify-start ">
+          <h2 className='text-left'>Vegetable Options 🥬</h2>
+          <div id='vegetables' className='flex justify-start '>
             {vegetablesOptions.map((item, i) => (
               <div key={i}>
-                <label className="inline-flex items-center mt-3">
+                <label className='inline-flex items-center mt-3'>
                   <input
                     id={item}
-                    type="checkbox"
-                    className="form-checkbox h-5 w-5 text-gray-600"
+                    type='checkbox'
+                    className='form-checkbox h-5 w-5 text-gray-600'
                     // onChange={() => {
                     //   handleCheckbox();
                     // }}
                     onChange={handleCheckbox}
                   />
-                  <span className="ml-2 text-gray-700">{item}</span>
+                  <span className='ml-2 text-gray-700'>{item}</span>
                 </label>
               </div>
             ))}
           </div>
           <br />
-          <div className="flex justify-start ">
+          <div className='flex justify-start '>
             <button
-              className="bg-orange-500 text-white px-4 py-2 rounded-md text-1xl font-medium hover:bg-blue-700 transition duration-300"
+              className='bg-orange-500 text-white px-4 py-2 rounded-md text-1xl font-medium hover:bg-blue-700 transition duration-300'
               onClick={(e) => {
                 sendChoices(e);
               }}
             >
-              Button 1
+              Make custom burrito bowl!
             </button>
           </div>
-          {order ? <div>{order}</div> : null}
+          {order === null
+            ? null
+            : order.map((order: any, index: string | number) => {
+                return (
+                  <div className='flex justify-start '>
+                    <li key={index}>{order}</li>
+                  </div>
+                );
+              })}
         </div>
       </form>
-      <div className="flex justify-start "></div>
+      <div className='flex justify-start '></div>
     </Fragment>
   );
 }
--- src/Functions/RandomOrder.tsx
+++ src/Functions/RandomOrder.tsx
@@ -1,34 +1,26 @@
-import { useState, Fragment, useEffect } from "react";
+import { useState, Fragment, useEffect } from 'react';
 
 function RandomOrder() {
-  const [protein, setProtein] = useState([""]);
-  const [bean, setBean] = useState([""]);
-  const [rice, setRice] = useState([""]);
-  const [sauce, setSauce] = useState([""]);
-  const [toppings, setToppings] = useState([""]);
-  const [vegetables, setVegetables] = useState([""]);
-  // const [randomOrder, setRandomOrder] = useState({});
-  // const [randomOrder, setRandomOrder] = useState("");
-  const [randomOrder, setRandomOrder] = useState({});
+  const [randomOrder, setRandomOrder] = useState<any | null>(null);
 
   const proteinOptions = [
-    "chicken",
-    "pollo asado",
-    "steak",
-    "barbacoa",
-    "carnitas",
-    "sofritas"
+    'chicken',
+    'pollo asado',
+    'steak',
+    'barbacoa',
+    'carnitas',
+    'sofritas',
   ];
-  const beanOptions = ["pinto beans", "black beans"];
-  const riceOptions = ["white rice", "brown rice"];
+  const beanOptions = ['pinto beans', 'black beans'];
+  const riceOptions = ['white rice', 'brown rice'];
   const sauceOptions = [
-    "pico de gallo",
-    "queso blanco",
-    "tomatillo-green chili salsa",
-    "tomatillo-red chili salsa"
+    'pico de gallo',
+    'queso blanco',
+    'tomatillo-green chili salsa',
+    'tomatillo-red chili salsa',
   ];
-  const toppingsOptions = ["cheese", "sour cream", "guacamole"];
-  const vegetablesOptions = ["fajita veggies", "corn salsa", "lettuce"];
+  const toppingsOptions = ['cheese', 'sour cream', 'guacamole'];
+  const vegetablesOptions = ['fajita veggies', 'corn salsa', 'lettuce'];
 
   function listIndex(array: string[]) {
     const max = array.length;
@@ -36,29 +28,7 @@ function RandomOrder() {
     return randomInt;
   }
 
-  // listIndex(proteinOptions);
-  // console.log(listIndex(proteinOptions));
-  // const menuOptions = [
-  //   proteinOptions,
-  //   beanOptions,
-  //   riceOptions,
-  //   sauceOptions,
-  //   toppingsOptions,
-  //   vegetablesOptions
-  // ];
-
-  // for (let i = 0; i < menuOptions.length; i++) {
-
-  // }
   function bowlRandomizer() {
-    // setProtein([proteinOptions[listIndex(proteinOptions)]]);
-    // setBean([beanOptions[listIndex(beanOptions)]]);
-    // setRice([riceOptions[listIndex(riceOptions)]]);
-    // setSauce([sauceOptions[listIndex(sauceOptions)]]);
-    // setToppings([toppingsOptions[listIndex(toppingsOptions)]]);
-    // setVegetables([vegetablesOptions[listIndex(vegetablesOptions)]]);
-    // setRandomOrder({ protein, bean, rice, sauce, toppings, vegetables });
-    // return randomOrder;
     let newOrder = [];
     newOrder.push(proteinOptions[listIndex(proteinOptions)]);
     newOrder.push(beanOptions[listIndex(beanOptions)]);
@@ -66,33 +36,32 @@ function RandomOrder() {
     newOrder.push(sauceOptions[listIndex(sauceOptions)]);
     newOrder.push(toppingsOptions[listIndex(toppingsOptions)]);
     newOrder.push(vegetablesOptions[listIndex(vegetablesOptions)]);
-
-    // return console.log(newOrder);
-    return newOrder;
+    setRandomOrder(newOrder);
   }
-  // console.log(JSON.stringify(order));
 
-  // useEffect(() => {
-  //   setRandomOrder(newOrder);
-  // }, []);
-
-  function randomBowlHandler() {
-    let tempOrder = bowlRandomizer();
-    setRandomOrder([tempOrder]);
-    return console.log(randomOrder);
-  }
+  useEffect(() => {
+    bowlRandomizer();
+  }, []);
 
   return (
     <Fragment>
       <div>
         <button
-          className="bg-orange-500 text-white px-4 py-2 rounded-md text-1xl font-medium hover:bg-blue-700 transition duration-300"
-          // onClick={bowlRandomizer}
-          onClick={randomBowlHandler}
+          className='bg-orange-500 text-white px-4 py-2 rounded-md text-1xl font-medium hover:bg-blue-700 transition duration-300'
+          onClick={bowlRandomizer}
+          // onClick={randomBowlHandler}
         >
           Make a randomized burrito bowl!
         </button>
-        {/* {randomOrder ? <div>{randomOrder}</div> : null} */}
+        {randomOrder === null
+          ? null
+          : randomOrder.map((randomOrder: any, index: string | number) => {
+              return (
+                <div className='flex justify-start '>
+                  <li key={index}>{randomOrder}</li>
+                </div>
+              );
+            })}
       </div>
     </Fragment>
   );
